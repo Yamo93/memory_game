947a6a66b1fbc539e1dcd19d6f96b9c8cf1c897a
Yamo Gebrewold <yamo.gebrewold@gmail.com>
2024-10-24 22:24:07 +0200

    render board

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import { words } from "./words.js";
 
 // create a board with random data
-const BOARD_WIDTH = 6;
-const BOARD_HEIGHT = 6;
+const BOARD_WIDTH = 4;
+const BOARD_HEIGHT = 4;
 
 if (BOARD_WIDTH % 2 === 1 && BOARD_HEIGHT % 2 === 1) {
     throw new Error("Odd width and height, invalid board size");
@@ -23,15 +23,43 @@ for (let i = randomWords.length - 1; i > 0; i--) {
     [randomWords[i], randomWords[j]] = [randomWords[j], randomWords[i]];
 }
 
+for (let i = 0; i < BOARD_HEIGHT; i++) {
+    const row: string[] = [];
+    board.push(row);
+    for (let j = 0; j < BOARD_WIDTH; j++) {
+        const randomWord = randomWords.pop();
+        if (randomWord) {
+            row.push(randomWord);
+        }
+    }
+}
 
-console.log(randomWords);
-
-console.log(board);
-
-// populate words and randomize them
+render(board);
 
 // create a player state
 
 // render board
 
 // add event listeners
+
+function render(board: string[][]): void {
+    const boardEl = document.createElement("div");
+    boardEl.setAttribute("id", "board");
+
+    for (let i = 0; i < board.length; i++) {
+        const row = board[i];
+        const rowEl = document.createElement("div");
+        rowEl.setAttribute("id", "row-" + i);
+        rowEl.classList.add("row");
+        for (let j = 0; j < row.length; j++) {
+            const word = row[j];
+            const wordEl = document.createElement("p");
+            wordEl.textContent = word;
+            wordEl.classList.add("word");
+            rowEl.appendChild(wordEl);
+        }
+        boardEl.appendChild(rowEl);
+    }
+
+    document.body.appendChild(boardEl);
+}
